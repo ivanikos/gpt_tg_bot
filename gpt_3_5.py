146dbeccd864d56159785d92c96cbf0ba177334d
ivanikos <vanikos@yandex.ru>
2023-03-19 14:00:10 +0900

    create bot on chat_gpt

---
+++ gpt_3_5.py
@@ -0,0 +1,43 @@
+import os
+import openai
+
+
+# openai.organization = "org-XNE0GNeBY8RQag2cQRbHzDzu"
+openai.api_key = "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"
+
+# openai.Model.list()
+
+prompt = "Придумай идею для телеграм бота, чтобы приносил прибыль. "
+
+
+# prompt = "праонализируй новости с какого-нибудь новостного сайта за сегодняшний день, приведи самую свежую новость" \
+#          " , приведи ссылку на эту новость и ссылку на фото относящееся к этой новости" # НЕ работает
+
+
+# prompt = "Придумай шутку на тему IT"
+
+
+def gpt_try(prompt):
+    print(prompt)
+
+    completion = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=[
+            {"role": "user", "content": f"{prompt}"}
+          ]
+        )
+    print("\n", completion.choices[0].message.content)
+    return completion.choices[0].message.content
+
+# answer = gpt_try(prompt)
+#
+# print(answer)
+
+
+
+
+
+
+
+
+
---
+++ gpt_testing_bot.py
@@ -0,0 +1,55 @@
+import logging
+
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+import gpt_3_5 as gpt
+
+
+
+btnHlp = KeyboardButton('Help')
+
+
+help_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False).row(btnHlp)
+
+bot = Bot(token='5905559198:AAFYJsIxxs08VWm4Moiqsjdz42p4tFV5grE')  # Токен моего  бота gpt_textai_bot
+
+dp: Dispatcher = Dispatcher(bot, storage=MemoryStorage())
+
+logging.basicConfig(level=logging.INFO)
+
+boss_id = 799592984
+
+
+@dp.message_handler(commands='start')
+async def start_using(message: types.Message):
+    if message.from_user.id == 799592984:
+        await message.answer(' Работает 19.03.2023')
+        await message.answer(' Чтобы задать вопрос нейросети Chat-GPT просто напиши в чат!', reply_markup=help_kb)
+
+    else:
+        await message.answer('Приветствую. Чтобы задать вопрос нейросети Chat-GPT просто напиши в чат!', reply_markup=help_kb)
+        await bot.send_message(799592984, f'Кто-то нажал старт user_id - {message.from_user.id}, \n'
+                                          f'user_name - {message.from_user.username}')
+
+
+
+@dp.message_handler()
+async def help_command(message: types.Message):
+    if message.from_user.id == 799592984:
+        if message.text == 'Help':
+            await message.answer('Работает пока что')
+            await message.answer('Чтобы задать вопрос нейросети Chat-GPT просто напиши в чат!',
+                                 reply_markup=help_kb)
+        else:
+            answer = gpt.gpt_try(message.text)
+            await message.answer(answer)
+
+    else:
+        answer = gpt.gpt_try(message.text)
+        await message.answer(answer)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
