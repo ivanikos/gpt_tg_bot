5430a5325afbbe399032bfca7c07e79242d28205
ivanikos <vanikos@yandex.ru>
2023-04-12 20:24:16 +0900

    ban users

--- gpt_testing_bot.py
+++ gpt_testing_bot.py
@@ -25,14 +25,20 @@ logging.basicConfig(level=logging.INFO)
 
 boss_id = 799592984
 
-ban_users = ['5895827248', '799592984']
+ban_users = ['5895827248']
 @dp.message_handler(commands='start')
 async def start_using(message: types.Message):
+    ban_users = []
+    with open("ban.txt", "r") as file:
+        banned_users = file.readlines()
+        for i in banned_users:
+            ban_users.append(i.replace('\n', '').replace(" ", ""))
+
     if str(message.from_user.id) in ban_users:
         await message.answer("You have banned.")
 
-    if message.from_user.id == 799592984:
-        await message.answer(' Работает 19.03.2023')
+    elif message.from_user.id == 799592984:
+        await message.answer(' Работает c 19.03.2023')
         await message.answer(' Чтобы задать вопрос нейросети Chat-GPT просто напиши в чат!', reply_markup=help_kb)
 
     else:
@@ -44,14 +50,47 @@ async def start_using(message: types.Message):
 
 @dp.message_handler()
 async def help_command(message: types.Message):
+    ban_users = []
+    with open("ban.txt", "r") as file:
+        banned_users = file.readlines()
+        for i in banned_users:
+            ban_users.append(i.replace('\n', '').replace(" ", ""))
+
     if str(message.from_user.id) in ban_users:
         await message.answer("You have banned.")
 
-    if message.from_user.id == 799592984:
+    elif message.from_user.id == 799592984:
         if message.text == 'Help':
             await message.answer('Работает несмотря ни на что')
             await message.answer('Чтобы задать вопрос нейросети Chat-GPT просто напиши в чат!',
                                  reply_markup=help_kb)
+
+        elif "ban" in str(message.text):
+            id_user_banned = str(message.text).replace(" ", "").replace("\n", "")
+            ban_users.append(id_user_banned)
+            with open("ban.txt", "w") as file:
+                for i in ban_users:
+                    file.write(i + "\n")
+
+            await message.answer(f"User id - {id_user_banned} - added in ban-list.")
+
+        elif "unban" in str(message.text):
+            id_user_unbanned = str(message.text).replace(" ", "").replace("\n", "")
+            ban_users.remove(id_user_unbanned)
+            with open("ban.txt", "w") as file:
+                for i in ban_users:
+                    file.write(i + "\n")
+            await message.answer(f"User id - {id_user_unbanned} - removed from ban-list.")
+
+
+        elif "ban-list" == message.text:
+            ban_list = ""
+            with open("ban.txt", "w") as file:
+                for i in ban_users:
+                    ban_list = ban_list + i + "\n"
+            await message.answer(f"Список заблокированных ID:\n {ban_list}")
+
+
         else:
             try:
                 answer = gpt.gpt_try(message.text)
