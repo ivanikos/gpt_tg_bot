ad04e48210150bd84b22a2ff227ff6e85b98ae31
ivanikos <vanikos@yandex.ru>
2023-11-18 20:25:54 -0600

    update API GPT

--- gpt_3_5.py
+++ gpt_3_5.py
@@ -5,30 +5,14 @@ import os
 
 openai.api_key = os.environ['OPENAI_API_KEY']
 
-# openai.Model.list()
-
-# prompt = "Придумай идею для телеграм бота, чтобы приносил прибыль. "
-
-
-# prompt = "праонализируй новости с какого-нибудь новостного сайта за сегодняшний день, приведи самую свежую новость" \
-#          " , приведи ссылку на эту новость и ссылку на фото относящееся к этой новости" # НЕ работает
-
-
-prompt = "Придумай шутку на тему IT"
-
-
 def gpt_try(prompt):
-    # print(str(prompt))
-
     try:
-
-        completion = openai.ChatCompletion.create(
+        completion = openai.chat.completions.create(
               model="gpt-3.5-turbo",
               messages=[
                 {"role": "user", "content": f"{prompt}"}
               ]
             )
-        # print(completion.choices[0].message.content)
         return [completion.choices[0].message.content, prompt]
     except Exception as exc:
         return [str(exc), "warning"]
